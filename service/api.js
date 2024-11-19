f107036dec75c0cb83190a507ed53fd4ef42419b
malshi sathsarani <125174511+malshiSathsarani-123@users.noreply.github.com>
2024-11-19 14:24:40 +0530

    connect backend

---
+++ service/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const BASE_URL = "http://localhost:5000/api/notes";
+
+// Function to save the note
+export const saveNote = async (title, note) => {
+    console.log(note)
+    try {
+        const response = await axios.post(BASE_URL, {
+            title: title,
+            note: note
+        });
+        return response.data; // Assuming the backend returns the saved note or success message
+    } catch (error) {
+        console.error("Error saving note:", error);
+        throw error; // Rethrow to handle in the calling component
+    }
+};
